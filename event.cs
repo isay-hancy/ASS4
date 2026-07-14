385a33f6283d36b082f2c2057da34930d79ffc8f
isay-hancy <aishakarki11111@gmail.com>
2026-07-14 19:06:05 +0545

    Added  Event.cs to ASS4

---
+++ event.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ASS4
+{
+    // Delegate
+    public delegate void MyEventHandler();
+
+    class EventDemo
+    {
+        // Event declaration
+        public event MyEventHandler MyEvent;
+
+        // Method to raise the event
+        public void RaiseEvent()
+        {
+            Console.WriteLine("Event is raised.");
+            MyEvent?.Invoke();
+        }
+    }
+
+    class Program
+    {
+        // Event handler
+        static void ShowMessage()
+        {
+            Console.WriteLine("Event handled successfully!");
+        }
+
+        static void Main(string[] args)
+        {
+            EventDemo obj = new EventDemo();
+
+            // Subscribe to the event
+            obj.MyEvent += ShowMessage;
+
+            // Raise the event
+            obj.RaiseEvent();
+
+            Console.ReadKey();
+        }
+    }
+}
